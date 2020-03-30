a5e0f7cf5c1ecda43f836d7e30dfa71917968127
SmallBasket42 <alexander.mukha23@gmail.com>
2020-03-30 15:18:39 +0300

    Домашнее задание к уроку 1

---
+++ src/Lesson_1/Road.java
@@ -0,0 +1,33 @@
+package Lesson_1;
+
+
+public class Road extends Barrier {
+    private int length;
+
+    public Road(String name, int length) {
+        super(name);
+
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    protected boolean moving(Actions actions) {
+        System.out.println( super.getName() + " length: " + this.length);
+
+        actions.run();
+
+        if (getLength() <= actions.getRunDistance()) {
+            System.out.println("run success");
+
+            return true;
+        } else {
+            System.out.println("run unsuccessfully");
+
+            return false;
+        }
+    }
+}
