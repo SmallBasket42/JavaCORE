965668e04941df44e7bf5a24b44c9bce38e78509
SmallBasket42 <alexander.mukha23@gmail.com>
2020-04-27 14:17:47 +0300

    Домашнее задание к уроку 8

---
+++ src/Lesson_8/Client/Callback.java
@@ -0,0 +1,5 @@
+package Lesson_8.Client;
+
+public interface Callback {
+    void callback(Object... args);
+}
---
+++ src/Lesson_8/Server/AuthService.java
@@ -0,0 +1,5 @@
+package Lesson_8.Server;
+
+public interface AuthService {
+    String getNickname(String login, String password);
+}
